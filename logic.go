1fcdc09ef86c056bb8291abe2fd9ff56fb3990b3
mohafarman <mohamadfarman977@gmail.com>
2025-05-09 02:40:12 +0200

    dealing initial cards

--- logic.go
+++ logic.go
@@ -16,9 +16,27 @@ type blackJack struct {
 	dealerHand []Card
 }
 
+func (bj blackJack) dealInitCards() {
+	var card Card
+	for range 2 {
+		/* Slicing slices to deal cards, deals one card at a time */
+		card = bj.deck.Cards[0]
+		bj.playerHand = append(bj.playerHand, card)
+		bj.deck.Cards = bj.deck.Cards[1:]
+
+		card = bj.deck.Cards[0]
+		bj.dealerHand = append(bj.dealerHand, card)
+		bj.deck.Cards = bj.deck.Cards[1:]
+	}
+}
+
 func newGame() blackJack {
-	return blackJack{
+	bj := blackJack{
 		gameState: ModeGameStart,
 		// TODO: Allow user to specify amount of decks to play with
-		deck: shuffleDeck(newDeck(1))}
+		deck: shuffleDeck(newDeck(1)),
+	}
+
+	bj.dealInitCards()
+	return bj
 }
